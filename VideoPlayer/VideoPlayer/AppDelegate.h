880b6eb0d8409e9ad5a92e1ff4b049f8e167e4a5
xinghun meng <xinghunm@curacloudcorp.com>
2016-11-30 13:50:43 +0800

    init the XHVideoPlayer repository

---
+++ VideoPlayer/VideoPlayer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  VideoPlayer
+//
+//  Created by intern08 on 11/30/16.
+//  Copyright © 2016 snow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ VideoPlayer/VideoPlayer/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  VideoPlayer
+//
+//  Created by intern08 on 11/30/16.
+//  Copyright © 2016 snow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
